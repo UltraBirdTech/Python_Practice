fd457f8690ed3f413fa077fd1c0f1926618a0673
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-04-13 12:04:16 +0900

    edit python method.

--- 03_fizzbuzz.py
+++ 03_fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1,100):
     string = ''
     if (i % 3) == 0:
-        string = string + 'fizz'
+        string += 'fizz'
     if (i % 5) == 0:
-        string = string + 'buzz'
+        string += 'buzz'
 
     if string != '':
         print(str(i) + ':' + string)
