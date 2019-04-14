d2438a38e60b24b7d976fb535557dd314f3d3397
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-04-14 10:20:22 +0900

    add comments.

--- 05_calculator.py
+++ 05_calculator.py
@@ -1,11 +1,13 @@
-
 import sys
 
+# recieve args.
 args = sys.argv
 first = int(args[1])
 second = int(args[2])
 operator = args[3]
+
 ans = 0
+
 if operator == '+':
     ans = first + second
 
@@ -18,7 +20,7 @@ elif operator == '*':
 elif operator == '/':
     # check zero
     if second == 0:
-        print('second is 0')
+        print('[ERROR]: second is 0')
         exit()
     ans = first /second
     
