f47afbff80bd53d5d172f78052742984a2fd0f07
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-04-15 06:44:40 +0900

    add description and error function.

--- 05_calculator.py
+++ 05_calculator.py
@@ -1,7 +1,23 @@
+##########calculator.py############################
+# 学ぶポイント
+# 1. 引数の扱い方
+# 2. 条件分岐のポイント
+# 3. 数値リテラルの四則演算
+###################################################
+
 import sys
 
 # recieve args.
 args = sys.argv
+
+print(len(args))
+# check args
+if len(args) <= 4:
+    print('[ERROR]:   引数の数が足りません')
+    print('[USAGE]:   python 05_calculator.py [最初の数値]、[二つ目の数値]、[計算方法(+-*/)]')
+    print('[EXAMPLE]: python 05_calculator.py 2 2 + ')
+    exit()
+
 first = int(args[1])
 second = int(args[2])
 operator = args[3]
@@ -23,5 +39,9 @@ elif operator == '/':
         print('[ERROR]: second is 0')
         exit()
     ans = first /second
+
+else:
+    print('[ERROR]: 演算子が期待したものではありません。+-*/のうちの一つからお選びください')
+    exit()
     
 print(ans)
