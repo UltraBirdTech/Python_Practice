060b37b3dce2fdd8eb39b408e9db6ea575714a4f
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-04-14 10:11:34 +0900

    add calculator method.

---
+++ 05_calculator.py
@@ -0,0 +1,25 @@
+
+import sys
+
+args = sys.argv
+first = int(args[1])
+second = int(args[2])
+operator = args[3]
+ans = 0
+if operator == '+':
+    ans = first + second
+
+elif operator == '-':
+    ans = first - second
+
+elif operator == '*':
+    ans = first * second
+
+elif operator == '/':
+    # check zero
+    if second == 0:
+        print('second is 0')
+        exit()
+    ans = first /second
+    
+print(ans)
