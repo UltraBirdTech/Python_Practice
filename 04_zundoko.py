1b96550d911959f441445264b0bfb8b6ff753c37
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-04-28 12:38:09 +0900

    edit

--- 04_zundoko.py
+++ 04_zundoko.py
@@ -1,13 +1,13 @@
 import random
 
-zundoko = ['zun', 'doko']
+zundoko = ['ズン', 'ドコ']
 array = []
 while(True):
     array.append(random.choice(zundoko))
     print(array[-1])
     if len(array) >= 5:
         str = ''.join(array)
-        if str == 'zunzunzunzundoko':
+        if str == 'ズンズンズンズンドコ':
             print(str)
             print('KI・YO・SHI')
             break
