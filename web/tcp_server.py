20481bfd09e018a4fcedcda4388a2291aac0ec57
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-05-03 18:10:19 +0900

    add tcp files

---
+++ web/tcp_server.py
@@ -0,0 +1,35 @@
+# --*--coding: utf-8 --*--
+
+import socket
+import threading
+
+bind_ip   = '0.0.0.0'
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip, bind_port))
+
+server.listen(5)
+
+print "[*] Listening on %s:%d" % (bind_ip, bind_port)
+
+# A thread of client connect.
+def handle_client(client_socket):
+    # display of client data
+    request = client_socket.recv(1024)
+
+    print "[*] Received: %s" % request
+
+    # send packet
+    client_socket.send("ACK!")
+
+    client_socket.close()
+
+while True:
+    client, addr = server.accept()
+    print "[*] Acceted connection from: %s:%d" % (addr[0], addr[1])
+
+    # A thread of recieve data
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
