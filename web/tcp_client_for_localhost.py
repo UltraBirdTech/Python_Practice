0776cdb7ba5695a37fd3b77e0508951326463891
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-05-03 18:13:37 +0900

    edit setting value.

--- web/tcp_client_for_localhost.py
+++ web/tcp_client_for_localhost.py
@@ -2,8 +2,8 @@
 
 import socket
 
-target_host = 'www.google.com'
-target_port = 80
+target_host = '127.0.0.1'
+target_port = 9999
 
 # crate socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +12,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # send data
-client.send(b"GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+#client.send(b"GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+client.send(b"ABCDEFG")
 
 # recieve data
 response = client.recv(4096)
