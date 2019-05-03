80dcfad26d090c8e588d12b840f4c20161df153d
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-05-03 18:12:14 +0900

    add a file about tcp client for localhost file.

---
+++ web/tcp_client_for_localhost.py
@@ -0,0 +1,20 @@
+# --*-- coding: utf-8 --*--
+
+import socket
+
+target_host = 'www.google.com'
+target_port = 80
+
+# crate socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to server
+client.connect((target_host, target_port))
+
+# send data
+client.send(b"GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+
+# recieve data
+response = client.recv(4096)
+
+print(response)
