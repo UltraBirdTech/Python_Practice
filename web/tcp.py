9999738f6c4cbe7fab5e80a903ba78c81f3c108c
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-05-03 18:11:33 +0900

    edit python 2 => python 3

--- web/tcp.py
+++ web/tcp.py
@@ -12,7 +12,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # send data
-client.send("GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
 
 # recieve data
 response = client.recv(4096)
