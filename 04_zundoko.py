f0f48f07cb631c9cdeb00f9c66bc71462e93af25
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-05-22 14:28:19 +0900

    edit

--- 04_zundoko.py
+++ 04_zundoko.py
@@ -6,9 +6,9 @@ while(True):
     array.append(random.choice(zundoko))
     print(array[-1])
     if len(array) >= 5:
-        str = ''.join(array)
-        if str == 'ズンズンズンズンドコ':
-            print(str)
+        s = ''.join(array)
+        if s == 'ズンズンズンズンドコ':
+            print(s)
             print('KI・YO・SHI')
             break
 
