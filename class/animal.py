966e9250c4f1ee498f1d788dea22551826a2ac84
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-05-28 08:02:30 +0900

    add some files.

---
+++ class/animal.py
@@ -0,0 +1,22 @@
+class Animal():
+    def __init__(self, name):
+        self.name = name
+
+    def cry(self):
+        print(self.voice)
+
+class Dog(Animal):
+    def __init__(self):
+        name = 'dog'
+        self.voice = 'Wow Wow'
+        super().__init__(name)
+
+class Cat(Animal):
+    def __init__(self):
+        name = 'Cat'
+        self.voice = 'Nya～'
+        super().__init__(name)
+
+Dog().cry()
+Cat().cry()
+
---
+++ class/taiyaki.py
@@ -0,0 +1,6 @@
+class Taiyaki():
+    def __init__(self, material):
+        print('このタイ焼きの中身は' + material + 'です')
+
+anko = Taiyaki('餡')
+custard = Taiyaki('カスタードクリーム')
