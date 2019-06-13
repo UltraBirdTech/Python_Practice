6f22dc487bd0db48a6ffb8e98b1bbf27541723fb
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-06-13 11:14:56 +0900

    delete varb i

--- 10_select_presenter.py
+++ 10_select_presenter.py
@@ -2,9 +2,8 @@ import random
 
 LineNum = 10
 members = ['M1', 'M2', 'M3']
-i = 1
 
-while len(members) != 0:
+while len(members):
     presenter = random.choice(members)
 
     string = ''
@@ -14,9 +13,8 @@ while len(members) != 0:
         for p in presenter:
             string += p + 'さん'
         
-    print(str(i) + '番目に発表する人は' + str(string) + 'です。' + ('\n' * LineNum))
+    print('次に発表する人は' + str(string) + 'です。' + ('\n' * LineNum))
         
     members.remove(presenter)
     print('残りの発表者数は' + str(len(members)) + '組です。')
-    i += 1
     input()
