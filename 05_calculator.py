87a2404677b107189f2560b8f2200a9471d85831
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-11-05 20:41:31 +0900

    edit comments

--- 05_calculator.py
+++ 05_calculator.py
@@ -17,15 +17,15 @@ def main():
         check_validate(argv)
         result = calculate(int(argv[1]), int(argv[2]), argv[3])
     except NotMatchArgvError as err:
-        print('[ERROR]:   引数の数が足りません')
+        print('[ERROR]:   引数の数が足りません。')
         print('[USAGE]:   python calculator.py [最初の数値][2つ目の数値][演算子(+-*/)]')
         print('[EXAMPLE]: python calculator.py 2 2 + ')
     except ValueError as err:
         print('[ERROR]: 計算する値は数値を入力してください。')
         print('[ERROR]: ', str(err))
     except NotIncludeError as err:
-        print('[ERROR]: 演算子が期待したものではありません。+-*/のうちの一つからお選びください')
-        print('[ERROR]: "', str(err), '" is not include a list')
+        print('[ERROR]: 演算子が期待したものではありません。+, -, *, / のうちの一つからお選びください。')
+        print('[ERROR]: "', str(err), '" is not include a list.')
     except ZeroDivisionError as err:
         print('[ERROR]: 割り算を行う際に0徐算が発生しました。第2引数を1以上の数値にしてください。')
         print('[ERROR]: ', str(err))
