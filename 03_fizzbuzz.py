7c5f5efe06f1af3e8707d2e6c65ae51d3ab07cb0
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-11-10 19:02:27 +0900

    edit

--- 03_fizzbuzz.py
+++ 03_fizzbuzz.py
@@ -4,15 +4,12 @@
 # 2. 文字列リテラルの扱い方
 # 3. 数字リテラルの余剰の出し方
 
-FIZZ = 'fizz'
-BUZZ = 'buzz'
-
 for i in range(1, 100):
     s = ''
     if (i % 3) == 0:
-        s += FIZZ
+        s += 'fizz'
     if (i % 5) == 0:
-        s += BUZZ
+        s += 'buzz'
 
     if s != '':
         print(str(i) + ':' + s)
