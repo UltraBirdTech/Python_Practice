73d6651e565320e5078cc59a9b7f633795736008
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-11-16 13:47:03 +0900

    add test file

---
+++ 05_calculator/tests/calculator_test.py
@@ -0,0 +1,78 @@
+# [USAGE]: python -m unittest tests/05_calculator_test.py
+import unittest
+import calculator
+from calculator import NotMatchArgvError
+from calculator import NotMatchNumError
+from calculator import NotIncludeError
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.argv = ['file_name', '5', '4', '+']
+
+    def test_check_validate(self):
+        calculator.check_validate(self.argv)
+
+    def test_invalid_not_match_value(self):
+        self.argv = []
+        with self.assertRaises(NotMatchArgvError):
+            calculator.check_validate(self.argv)
+    
+    def test_invalid_first_value(self):
+        self.argv[1] = 'a'
+        with self.assertRaises(NotMatchNumError):
+            calculator.check_validate(self.argv)
+
+    def test_invalid_second_value(self):
+        self.argv[2] = 'a'
+        with self.assertRaises(NotMatchNumError):
+            calculator.check_validate(self.argv)
+
+    def test_not_include_error_value(self):
+        self.argv[3] = 'a'
+        with self.assertRaises(NotIncludeError):
+            calculator.check_validate(self.argv)
+
+
+class TestCheckDigit(unittest.TestCase):
+    def test_check_true(self):
+        self.assertEqual(calculator.check_digit(str(1)), True)
+
+    def test_check_true_minus(self):
+        self.assertEqual(calculator.check_digit(str(-1)), True)
+
+    def test_check_false(self):
+        self.assertEqual(calculator.check_digit('a'), False)
+
+
+class TestCaluculate(unittest.TestCase):
+    def setUp(self):
+        self.first = 8
+        self.second = 4
+        self.operator = '+'
+
+    def test_caluculate_plus(self):
+        self.assertEqual(calculator.calculate(self.first, self.second, self.operator), 12) 
+
+    def test_caluculate_minus(self):
+        self.operator = '-'
+        self.assertEqual(calculator.calculate(self.first, self.second, self.operator), 4) 
+
+    def test_caluculate_multiplication(self):
+        self.operator = '*'
+        self.assertEqual(calculator.calculate(self.first, self.second, self.operator), 32)
+
+    def test_caluculate_division(self):
+        self.operator = '/'
+        self.assertEqual(calculator.calculate(self.first, self.second, self.operator), 2)
+
+    def test_caluculate_zero_division_error(self):
+        self.second = 0
+        self.operator = '/'
+        with self.assertRaises(ZeroDivisionError):
+            calculator.calculate(self.first, self.second, self.operator)
+
+    def test_caluculate_not_include_error(self):
+        self.operator = 'a'
+        with self.assertRaises(NotIncludeError):
+            calculator.calculate(self.first, self.second, self.operator)
+
