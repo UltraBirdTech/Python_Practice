acd4c5986c296d47766e5e2b3fd1e30de1bf3e4c
UltraBirdTech <ultrabirdrepti@gmail.com>
2020-08-18 08:18:09 +0900

    add main.py

---
+++ module_import/main.py
@@ -0,0 +1,8 @@
+from child_first import ChildFirst
+from child_second import ChildSecond
+
+child_first = ChildFirst()
+child_first.parent_method()
+
+child_second = ChildSecond()
+child_second.parent_method()
