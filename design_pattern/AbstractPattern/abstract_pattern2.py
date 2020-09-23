34e1c052e3a7094fa9507c9dabbf9acdff00f59d
UltraBirdTech <ultrabirdrepti@gmail.com>
2020-09-24 08:47:21 +0900

    verb dict

--- design_pattern/AbstractPattern/abstract_pattern2.py
+++ design_pattern/AbstractPattern/abstract_pattern2.py
@@ -11,6 +11,7 @@ def main():
 
 def make_pizza(PizzaFactory, amount_str):
     pizza = PizzaFactory()
+    amountdict = {}
     mount = amountdict[amount_str]
     pizza.pizza_matterials = []
     pizza.pizza_matterials.append(piiza.add_dough(amount))
