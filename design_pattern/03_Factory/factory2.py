3902a9a55bb28d3b3ef51f67faff70692a54e3bc
UltraBirdTech <ultrabirdrepti@gmail.com>
2020-11-29 10:39:58 +0900

    add factory 2.py

---
+++ design_pattern/03_Factory/factory2.py
@@ -0,0 +1,41 @@
+import abc
+
+def main():
+    pass
+
+class Factory:
+    pass
+
+class Animal:
+    @abc.abstractmethod
+    def eat(self):
+        pass
+
+    @abc.abstractmethod
+    def speak(self):
+        pass
+
+class CowFactory(Factory):
+    def factory_method(self):
+        return Cow()
+
+class ChickenFactory(Factory):
+    def factory_method(self):
+        return Chicken()
+
+class Cow(Animal):
+    def eat(self):
+        print('Cow:eat')
+
+    def speak(self):
+        print('Cow:speak')
+
+class Chicken(Animal):
+    def eat(self):
+        print('Chicken:eat')
+
+    def speak(self):
+        print('Chicken:speak')
+
+if __name__ == '__main__':
+    main()
