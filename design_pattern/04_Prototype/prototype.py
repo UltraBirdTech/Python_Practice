b03b61351638b0957604253b2d26d1ae0be42205
UltraBirdTech <ultrabirdrepti@gmail.com>
2020-12-13 11:22:15 +0900

    add y method

--- design_pattern/04_Prototype/prototype.py
+++ design_pattern/04_Prototype/prototype.py
@@ -4,5 +4,6 @@ def main():
     point1 = Point(2, 3)
     point2 = copy.deepcopy(point1)
 
-    print("x", point1.x, point2.y)
+    print("x", point1.x, point1.y)
+    print("y", point1.x, point2.y)
 
