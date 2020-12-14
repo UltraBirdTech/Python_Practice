fca4e7f1d810f223cdc17d03fbe030779775082d
UltraBirdTech <ultrabirdrepti@gmail.com>
2020-12-14 19:13:00 +0900

    add

--- design_pattern/04_Prototype/prototype.py
+++ design_pattern/04_Prototype/prototype.py
@@ -7,3 +7,11 @@ def main():
     print("x", point1.x, point1.y)
     print("y", point1.x, point2.y)
 
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+if __name__ == "__main__":
+    main()
+        
